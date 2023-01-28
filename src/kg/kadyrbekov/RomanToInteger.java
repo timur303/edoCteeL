cea9d96452ebdcddb88db23fb95d2fceb77eefe8
Тимур <kadyrbekovtimur001@gmail.com>
2023-01-28 15:25:12 +0600

    Initial commit

---
+++ src/kg/kadyrbekov/RomanToInteger.java
@@ -0,0 +1,43 @@
+package kg.kadyrbekov;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class RomanToInteger {
+
+    public static void main(String[] args) {
+        System.out.println(romanToInt("d"));
+    }
+
+    public static int romanToInt(String j) {
+
+        Map<Character, Integer> map = new HashMap<>();
+        {
+            map.put('I',1);
+            map.put('V',5);
+            map.put('X',10);
+            map.put('L', 50);
+            map.put('C', 100);
+            map.put('D', 500);
+            map.put('M',1000);
+        }
+
+
+        int result = 0;
+        int prev = 0;
+        for(int i = j.length() -1; i >= 0; i-- ){
+            int current = map.get(j.toUpperCase(Locale.ROOT).charAt(i));
+            if(current<prev){
+                result -= current;
+            }else{
+                result+=current;
+            }
+            prev = current;
+
+        }
+        return result;
+
+    }
+
+}
