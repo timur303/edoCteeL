7bdc9b2e963c558d5263d401247a2c2d4278d488
Тимур <kadyrbekovtimur001@gmail.com>
2023-02-24 23:50:35 +0600

    Merge remote-tracking branch 'origin/master'

---
+++ src/kg/kadyrbekov/programmingSkills/MergeString.java
@@ -0,0 +1,10 @@
+package kg.kadyrbekov.programmingSkills;
+
+public class MergeString {
+    public static void main(String[] args) {
+
+    }
+//    public String mergeAlternately(String word1, String word2) {
+//
+//    }
+}
